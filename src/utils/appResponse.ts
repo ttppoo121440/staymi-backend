2be0627511b202573f448e9d8899aa320cf0087f
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-12 16:07:41 +1000

    refactor: 移除 appError 函式並簡化 NotFound 回應邏輯

--- src/utils/appResponse.ts
+++ src/utils/appResponse.ts
@@ -1,9 +1,7 @@
-import type { Request, Response, NextFunction } from 'express';
+import type { Request, Response } from 'express';
 
 import type { ApiResponse } from '@/types/ApiResponseType';
-import type { AppError } from '@/types/AppErrorType';
 
-import { ErrorCode, ErrorStatus } from './errorCode';
 import logger from './logger';
 
 export const successResponse = <T>(data: T, message = '請求成功'): ApiResponse<T> => {
@@ -32,23 +30,9 @@ export function errorResponse<T = null>(message: string, data: T = null as T): A
  */
 export const NotFound = (req: Request, res: Response): void => {
   logger.error(`[${req.method}] 404 : ${req.path}`);
-  res.status(404).json(errorResponse('查無此路由，請確認 API 格式!'));
-};
-
-/**
- * Creates an application error and passes it to the next middleware.
- *
- * 此函式用來產生一個包含自訂訊息與狀態碼的錯誤，並將該錯誤傳遞給 Express 的錯誤處理中介軟體。
- *
- * @param {string} errMessage - 錯誤訊息。
- * @param {NextFunction} next - Express 的 next 函式，用於傳遞錯誤至下一個中介軟體。
- * @param {number} [httpStatus=400] - HTTP 狀態碼（預設為 400）。
- */
-export const appError = (errMessage: string, next: NextFunction, httpStatus = 400): void => {
-  const error = new Error(errMessage) as AppError;
-  error.statusCode = httpStatus;
-  error.isOperational = true;
-  error.status = httpStatus == 500 ? ErrorStatus[ErrorCode.INTERNAL_SERVER_ERROR] : ErrorStatus[ErrorCode.BAD_REQUEST];
-
-  next(error);
+  res.status(404).json({
+    success: false,
+    message: '查無此路由，請確認 API 格式!',
+    data: null,
+  });
 };
