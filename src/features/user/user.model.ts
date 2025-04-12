1aa97350feb4c3b9f0eb1b209e589599c61508af
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-12 16:38:20 +1000

    chore: 刪除不必要的檔案

--- src/features/user/user.model.ts
+++
@@ -1,6 +0,0 @@
-import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
-
-import { user } from '@/database/schemas/user.schema';
-
-export const insertUserSchema = createInsertSchema(user);
-export const selectUserSchema = createSelectSchema(user);
