d454a179f433c04e9aa4c04fb0d66ca15682cfc1
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-19 00:31:45 +1000

    feat: 新增 /api/v1/admin/users 路由 平台管理使用的API

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import type { Application } from 'express';
 
+import adminUserRoutes from './features/adminUser/adminUser.routes';
 import userRoutes from './features/user/user.routes';
 import { NotFound } from './utils/appResponse';
 
 export const setupRoutes = (app: Application): void => {
+  app.use('/api/v1/admin/users', adminUserRoutes);
   app.use('/api/v1/users', userRoutes);
   app.all('*', NotFound);
 };
