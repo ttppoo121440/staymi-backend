4623d65fa9c8adce9dd8bb7d10079a34653bc6e4
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-20 15:35:56 +1000

    feat: 新增 /api/v1/store 路由 商店註冊 更改商家資訊

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import type { Application } from 'express';
 
 import adminUserRoutes from './features/adminUser/adminUser.routes';
+import authStoreRoutes from './features/authStore/authStore.routes';
 import userRoutes from './features/user/user.routes';
 import { NotFound } from './utils/appResponse';
 
 export const setupRoutes = (app: Application): void => {
   app.use('/api/v1/admin/users', adminUserRoutes);
   app.use('/api/v1/users', userRoutes);
+  app.use('/api/v1/store', authStoreRoutes);
   app.all('*', NotFound);
 };
