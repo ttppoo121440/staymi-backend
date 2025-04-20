603c4c091a8eaef5941f72a8c4af8191ab78b227
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-20 15:41:16 +1000

    feat: 新增 AuthStoreController，包含商家註冊與更新商店資訊功能

---
+++ src/features/authStore/authStore.controller.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { HttpStatus } from '@/types/http-status.enum';
+import { JwtUserPayload } from '@/types/JwtUserPayload';
+import { successResponse } from '@/utils/appResponse';
+import logger from '@/utils/logger';
+
+import { AuthRepo } from '../auth/auth.repo';
+
+import { AuthStoreRepo } from './authStore.repo';
+
+export class AuthStoreController {
+  private authStoreRepo = new AuthStoreRepo();
+  private authRepo = new AuthRepo();
+  public async signup(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const userData = req.body;
+      const newUser = await this.authStoreRepo.signup(userData);
+      const token = await this.authRepo.login({ email: newUser.email, password: userData.password });
+      const userInfo = await this.authRepo.getUserByEmail(newUser.email);
+      const responseData = {
+        token,
+        user: {
+          name: userInfo.name,
+          avatar: userInfo.avatar,
+        },
+      };
+      res.status(HttpStatus.CREATED).json(successResponse(responseData, '註冊成功'));
+    } catch (error) {
+      logger.error('註冊失敗:', error);
+      next(error);
+    }
+  }
+  async updateStoreInfo(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const userData = req.body;
+      const userId: string = (req.user as JwtUserPayload).id;
+      const updatedUser = await this.authStoreRepo.updateStoreInfo(userId, userData);
+      res.status(HttpStatus.OK).json(successResponse(updatedUser, '更新成功'));
+    } catch (error) {
+      logger.error('更新失敗:', error);
+      next(error);
+    }
+  }
+}
