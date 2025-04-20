5c47d2fd613393dc22ea8bad9a63f01e92445828
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-21 00:49:48 +1000

    feat: 新增 createHotel 方法處理酒店創建邏輯

---
+++ src/features/storeHotel/storeHotel.controller.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { HttpStatus } from '@/types/http-status.enum';
+import { successResponse } from '@/utils/appResponse';
+import logger from '@/utils/logger';
+
+import { StoreHotelRepo } from './storeHotel.repo';
+import { hotelCreateSchema } from './storeHotel.schema';
+
+export class StoreHotelController {
+  private storeHotelRepo = new StoreHotelRepo();
+
+  async createHotel(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const validatedData = hotelCreateSchema.parse(req.body);
+      const newHotel = await this.storeHotelRepo.create(validatedData);
+      res.status(HttpStatus.CREATED).json(successResponse(newHotel, '酒店創建成功'));
+    } catch (error) {
+      logger.error('酒店創建失敗:', error);
+      next(error);
+    }
+  }
+}
