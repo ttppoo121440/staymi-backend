6071e9e09fdfcbd851af93a447d8cd9cc9635801
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-21 00:45:13 +1000

    feat: 新增 storeHotel 的路由

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import type { Application } from 'express';
 
 import adminUserRoutes from './features/adminUser/adminUser.routes';
 import authStoreRoutes from './features/authStore/authStore.routes';
+import storeHotelRoutes from './features/storeHotel/storeHotel.routes';
 import userRoutes from './features/user/user.routes';
 import { NotFound } from './utils/appResponse';
 
@@ -9,5 +10,6 @@ export const setupRoutes = (app: Application): void => {
   app.use('/api/v1/admin/users', adminUserRoutes);
   app.use('/api/v1/users', userRoutes);
   app.use('/api/v1/store', authStoreRoutes);
+  app.use('/api/v1/store/hotel', storeHotelRoutes);
   app.all('*', NotFound);
 };
