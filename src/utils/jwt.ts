984ebed57e6b2c2bd51e07e19a298ac9e7150f6f
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-21 16:44:05 +1000

    feat: 將 JWT 的過期時間從 7 天延長至 30 天

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -13,6 +13,6 @@ export const generateToken = (payload: Payload): string => {
   if (!process.env.JWT_SECRET) throw new Error('JWT_SECRET not set');
 
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: '7d',
+    expiresIn: '30d',
   });
 };
