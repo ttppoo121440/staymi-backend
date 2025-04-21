5d7a3c99767a1b81c40ba044dda34e93a1d7aa0a
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-21 16:34:55 +1000

    feat: 新增 upload 的 API 路由

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import type { Application } from 'express';
 import adminUserRoutes from './features/adminUser/adminUser.routes';
 import authStoreRoutes from './features/authStore/authStore.routes';
 import storeHotelRoutes from './features/storeHotel/storeHotel.routes';
+import uploadRoutes from './features/upload/upload.routes';
 import userRoutes from './features/user/user.routes';
 import { NotFound } from './utils/appResponse';
 
@@ -11,5 +12,6 @@ export const setupRoutes = (app: Application): void => {
   app.use('/api/v1/users', userRoutes);
   app.use('/api/v1/store', authStoreRoutes);
   app.use('/api/v1/store/hotel', storeHotelRoutes);
+  app.use('/api/v1/upload', uploadRoutes);
   app.all('*', NotFound);
 };
