aafb5b43cd6628177a0946de721d88ac68ab180d
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-27 17:23:56 +1000

    refactor: 移除 JwtUserPayload 中的 email 欄位

--- src/types/JwtUserPayload.ts
+++ src/types/JwtUserPayload.ts
@@ -1,4 +1,4 @@
 export type JwtUserPayload =
-  | { id: string; email: string; role: 'consumer' }
-  | { id: string; email: string; role: 'store'; brand_id: string }
-  | { id: string; email: string; role: 'admin' };
+  | { id: string; role: 'consumer' }
+  | { id: string; role: 'store'; brand_id: string }
+  | { id: string; role: 'admin' };
