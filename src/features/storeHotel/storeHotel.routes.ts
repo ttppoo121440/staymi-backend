15c5603012f47190a6149760bb13ddb5c751bd8c
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-27 14:22:56 +1000

    feat: 新增 StoreHotel 路由，包含取得、建立及更新飯店功能

--- src/features/storeHotel/storeHotel.routes.ts
+++ src/features/storeHotel/storeHotel.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { authMiddleware } from '@/middleware/auth.middleware';
+import { authBrandId } from '@/middleware/authBrandId.middleware';
 import { checkRolesMiddleware } from '@/middleware/checkRoles.middleware';
 
 import { StoreHotelController } from './storeHotel.controller';
@@ -8,11 +9,34 @@ import { StoreHotelController } from './storeHotel.controller';
 const storeHotelRoutes = express.Router();
 const storeHotelController = new StoreHotelController();
 
+storeHotelRoutes.get(
+  '/',
+  authMiddleware,
+  checkRolesMiddleware(['store', 'admin']),
+  authBrandId,
+  storeHotelController.getAll,
+);
+storeHotelRoutes.get(
+  '/:id',
+  authMiddleware,
+  checkRolesMiddleware(['store', 'admin']),
+  authBrandId,
+  storeHotelController.getById,
+);
 storeHotelRoutes.post(
   '/',
   authMiddleware,
   checkRolesMiddleware(['store', 'admin']),
-  storeHotelController.createHotel.bind(storeHotelController),
+  authBrandId,
+  storeHotelController.create,
+);
+
+storeHotelRoutes.put(
+  '/:id',
+  authMiddleware,
+  checkRolesMiddleware(['store', 'admin']),
+  authBrandId,
+  storeHotelController.update,
 );
 
 export default storeHotelRoutes;
