bad158c66f7c4d5661aacac15e846f354dba099c
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-27 17:22:19 +1000

    style: 調整路由的順序以改善可讀性

--- src/routes.ts
+++ src/routes.ts
@@ -10,8 +10,8 @@ import { NotFound } from './utils/appResponse';
 export const setupRoutes = (app: Application): void => {
   app.use('/api/v1/admin/users', adminUserRoutes);
   app.use('/api/v1/users', userRoutes);
-  app.use('/api/v1/store', authStoreRoutes);
   app.use('/api/v1/store/hotel', storeHotelRoutes);
+  app.use('/api/v1/store', authStoreRoutes);
   app.use('/api/v1/upload', imageUploadRoutes);
   app.all('*', NotFound);
 };
