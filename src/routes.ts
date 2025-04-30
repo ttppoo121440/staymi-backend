97ae9bdca32cf3eaac4dde751b02c9a7e5b54e49
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-30 22:36:01 +1000

    feat: 新增 roomType 路由

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import type { Application } from 'express';
 import adminUserRoutes from './features/adminUser/adminUser.routes';
 import authStoreRoutes from './features/authStore/authStore.routes';
 import imageUploadRoutes from './features/imageUpload/imageUpload.routes';
+import roomTypeRoutes from './features/roomType/roomType.routes';
 import storeHotelRoutes from './features/storeHotel/storeHotel.routes';
 import userRoutes from './features/user/user.routes';
 import { NotFound } from './utils/appResponse';
@@ -10,6 +11,7 @@ import { NotFound } from './utils/appResponse';
 export const setupRoutes = (app: Application): void => {
   app.use('/api/v1/admin/users', adminUserRoutes);
   app.use('/api/v1/users', userRoutes);
+  app.use('/api/v1/store/hotel/room-type', roomTypeRoutes);
   app.use('/api/v1/store/hotel', storeHotelRoutes);
   app.use('/api/v1/store', authStoreRoutes);
   app.use('/api/v1/upload', imageUploadRoutes);
