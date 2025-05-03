29e887bb866114b068d70425c7aa41b45ed793e2
ttppoo121440 <ttppoo121440@gmail.com>
2025-05-03 16:03:07 +1000

    feat: 新增 products 資料表的模式定義

---
+++ src/database/schemas/products.schema.ts
@@ -0,0 +1,16 @@
+import { pgTable, text, uuid, varchar, timestamp } from 'drizzle-orm/pg-core';
+
+import { hotels } from './hotels.schema';
+
+export const products = pgTable('products', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  hotel_id: uuid('hotel_id')
+    .notNull()
+    .references(() => hotels.id),
+  name: varchar('name', { length: 50 }).notNull(),
+  features: varchar('features', { length: 255 }).notNull(),
+  description: text('description').notNull(),
+  imageUrl: varchar('imageUrl', { length: 255 }).notNull(),
+  created_at: timestamp('created_at').defaultNow(),
+  updated_at: timestamp('updated_at').defaultNow(),
+});
