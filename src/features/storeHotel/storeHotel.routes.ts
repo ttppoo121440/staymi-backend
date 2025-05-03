2ff0d546e17ec608897b0b9303246da35d56d775
ttppoo121440 <ttppoo121440@gmail.com>
2025-05-03 12:15:11 +1000

    feat: 新增 hotelImageController 相關路由

--- src/features/storeHotel/storeHotel.routes.ts
+++ src/features/storeHotel/storeHotel.routes.ts
@@ -4,10 +4,50 @@ import { authMiddleware } from '@/middleware/auth.middleware';
 import { authBrandId } from '@/middleware/authBrandId.middleware';
 import { checkRolesMiddleware } from '@/middleware/checkRoles.middleware';
 
+import { HotelImageController } from '../hotelImage/hotelImage.controller';
+
 import { StoreHotelController } from './storeHotel.controller';
 
 const storeHotelRoutes = express.Router();
 const storeHotelController = new StoreHotelController();
+const hotelImageController = new HotelImageController();
+
+storeHotelRoutes.get(
+  '/:hotelId/images',
+  authMiddleware,
+  checkRolesMiddleware(['store', 'admin']),
+  authBrandId,
+  hotelImageController.getAll,
+);
+storeHotelRoutes.post(
+  '/:hotelId/images',
+  authMiddleware,
+  checkRolesMiddleware(['store', 'admin']),
+  authBrandId,
+  hotelImageController.create,
+);
+storeHotelRoutes.get(
+  '/:hotelId/images/:id',
+  authMiddleware,
+  checkRolesMiddleware(['store', 'admin']),
+  authBrandId,
+  hotelImageController.getById,
+);
+storeHotelRoutes.put(
+  '/:hotelId/images/:id',
+  authMiddleware,
+  checkRolesMiddleware(['store', 'admin']),
+  authBrandId,
+  hotelImageController.update,
+);
+
+storeHotelRoutes.delete(
+  '/:hotelId/images/:id',
+  authMiddleware,
+  checkRolesMiddleware(['store', 'admin']),
+  authBrandId,
+  hotelImageController.delete,
+);
 
 storeHotelRoutes.get(
   '/',
