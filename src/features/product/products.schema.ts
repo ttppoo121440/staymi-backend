b82e80670fa2e31e770112d9f326025736b97c23
ttppoo121440 <ttppoo121440@gmail.com>
2025-05-04 00:31:36 +1000

    feat: 新增 productsSchema 和相關 Zod schema

---
+++ src/features/product/products.schema.ts
@@ -0,0 +1,54 @@
+import { z } from 'zod';
+
+import { paginationSchema } from '@/types/pagination';
+import { formatDisplayDate, zDateOrDefault } from '@/utils/formatDate';
+
+export const productsSchema = z.object({
+  id: z.string().uuid(),
+  hotel_id: z.string().uuid(),
+  name: z.string().min(1, '請輸入伴手禮名稱').max(50),
+  features: z.string({ message: '請輸入伴手禮特色' }).max(255),
+  description: z.string({ message: '請輸入伴手禮描述' }).max(255),
+  imageUrl: z.string({ message: '請選擇伴手禮圖片' }),
+  created_at: zDateOrDefault(),
+  updated_at: zDateOrDefault(),
+});
+
+export const productsListDto = z
+  .object({
+    products: z.array(productsSchema),
+    pagination: paginationSchema,
+  })
+  .transform((data) => ({
+    products: data.products.map((product) => ({
+      ...product,
+      created_at: formatDisplayDate(product.created_at, 'YYYY-MM-DD HH:mm:ss'),
+      updated_at: formatDisplayDate(product.updated_at, 'YYYY-MM-DD HH:mm:ss'),
+    })),
+    pagination: data.pagination,
+  }));
+
+export const productsDto = z
+  .object({
+    product: productsSchema,
+  })
+  .transform((data) => ({
+    product: {
+      ...data.product,
+      created_at: formatDisplayDate(data.product.created_at, 'YYYY-MM-DD HH:mm:ss'),
+      updated_at: formatDisplayDate(data.product.updated_at, 'YYYY-MM-DD HH:mm:ss'),
+    },
+  }));
+
+export const productsCreateSchema = productsSchema.omit({
+  id: true,
+  created_at: true,
+  updated_at: true,
+});
+export const productsUpdateSchema = productsSchema.omit({
+  created_at: true,
+});
+
+export type ProductsSchema = z.infer<typeof productsSchema>;
+export type ProductsCreateType = z.infer<typeof productsCreateSchema>;
+export type ProductsUpdateType = z.infer<typeof productsUpdateSchema>;
