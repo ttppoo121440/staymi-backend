0b9a73f000e8a8b9756d41da40a7d055b9c39206
ttppoo121440 <ttppoo121440@gmail.com>
2025-05-04 19:48:47 +1000

    chore: 移除未使用的 isDatabaseError.ts 檔案

--- src/utils/isDatabaseError.ts
+++
@@ -1,8 +0,0 @@
-export function isDatabaseError(error: unknown): error is { code: string } {
-  return (
-    typeof error === 'object' &&
-    error !== null &&
-    'code' in error &&
-    typeof (error as Record<string, unknown>).code === 'string'
-  );
-}
