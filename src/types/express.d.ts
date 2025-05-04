2e815828eb26fb919331bffc2d65a7867e7c2f7a
ttppoo121440 <ttppoo121440@gmail.com>
2025-05-04 19:48:11 +1000

    feat: 擴展 Express.User 類型以支援自定義使用者屬性

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,6 +3,8 @@ import type { JwtUserPayload } from './JwtUserPayload';
 
 declare global {
   namespace Express {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface User extends SerializedUser {} // 擴展 User 類型
     interface Request {
       user: JwtUserPayload;
       brand_id: string;
