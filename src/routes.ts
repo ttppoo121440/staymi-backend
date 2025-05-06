e13527a8319e9e99e2d236a9889b49550ea2e3f2
chindesu0207 <147689292+chindesu0207@users.noreply.github.com>
2025-05-06 23:16:54 +0800

    feat: 🎸 加入 room-plan, product-plan 路由

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import adminUserRoutes from './features/adminUser/adminUser.routes';
 import authStoreRoutes from './features/authStore/authStore.routes';
 import hotelRoomRoutes from './features/hotelRoom/hotelRoom.routes';
 import imageUploadRoutes from './features/imageUpload/imageUpload.routes';
+import productPlanRoutes from './features/productPlan/productPlan.routes';
+import roomPlanRoutes from './features/roomPlan/roomPlan.routes';
 import roomTypeRoutes from './features/roomType/roomType.routes';
 import storeHotelRoutes from './features/storeHotel/storeHotel.routes';
 import userRoutes from './features/user/user.routes';
@@ -14,6 +16,8 @@ export const setupRoutes = (app: Application): void => {
   app.use('/api/v1/users', userRoutes);
   app.use('/api/v1/store/hotel/room-type', roomTypeRoutes);
   app.use('/api/v1/store/hotel/hotel-rooms', hotelRoomRoutes);
+  app.use('/api/v1/store/hotel/room-plan', roomPlanRoutes);
+  app.use('/api/v1/store/hotel/product-plan', productPlanRoutes);
   app.use('/api/v1/store/hotel', storeHotelRoutes);
   app.use('/api/v1/store', authStoreRoutes);
   app.use('/api/v1/upload', imageUploadRoutes);
