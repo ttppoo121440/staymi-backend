3fd2b488c2f4a5fb514073f6c3649c14690e19b8
KK <kpro8899@protonmail.com>
2025-05-14 22:01:11 +0800

    feat: 取消訂閱 - add 參數錯誤回應

--- src/features/subscription/subscription.routes.ts
+++ src/features/subscription/subscription.routes.ts
@@ -10,6 +10,7 @@ const subscriptionController = new SubscriptionController();
 // 查詢個人訂閱狀態
 subscriptionRoutes.get('/', authMiddleware, subscriptionController.getPlan);
 
+// 取消自動訂閱狀態
 subscriptionRoutes.put('/is-recurring', authMiddleware, subscriptionController.updateIsRecurring);
 
 export default subscriptionRoutes;
