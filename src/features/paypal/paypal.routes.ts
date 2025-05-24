7b93db8582a2aed4a7a95a5c593705641c44f542
ttppoo121440 <ttppoo121440@gmail.com>
2025-05-24 13:15:05 +1000

    feat: PayPal 路由新增驗證與 schema 驗證

--- src/features/paypal/paypal.routes.ts
+++ src/features/paypal/paypal.routes.ts
@@ -1,11 +1,31 @@
 import { Router } from 'express';
 
+import { authMiddleware } from '@/middleware/auth.middleware';
+import { zodMiddleware } from '@/middleware/zodMiddleware';
+
+import { orderRoomProductCreateSchema } from '../orderRoomProduct/orderRoomProduct.schema';
+
 import { PayPalController } from './paypal.controller';
+import { paypalSchema } from './paypal.schema';
 
 const paypalRouter = Router();
 const payPalController = new PayPalController();
 
-paypalRouter.post('/create-order', payPalController.createPayPalOrder);
-paypalRouter.post('/capture-order/:orderId', payPalController.capturePayPalOrder);
+paypalRouter.post(
+  '/create-order',
+  authMiddleware,
+  zodMiddleware({
+    body: orderRoomProductCreateSchema,
+  }),
+  payPalController.createPayPalOrder,
+);
+paypalRouter.post(
+  '/capture-order/:id',
+  authMiddleware,
+  zodMiddleware({
+    body: paypalSchema,
+  }),
+  payPalController.capturePayPalOrder,
+);
 
 export default paypalRouter;
