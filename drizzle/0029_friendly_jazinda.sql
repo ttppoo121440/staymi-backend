c1787e1ac83a4ad36ba1fad854e7e55fe68c6857
ttppoo121440 <ttppoo121440@gmail.com>
2025-05-24 12:38:04 +1000

    feat: order_room_product 新增 paypal_order_id 與 paypal_transaction_id 欄位

---
+++ drizzle/0029_friendly_jazinda.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "order_room_product" ADD COLUMN "paypal_order_id" varchar(100);--> statement-breakpoint
+ALTER TABLE "order_room_product" ADD COLUMN "paypal_transaction_id" varchar(100);
