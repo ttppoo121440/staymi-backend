c1d933f0c8f21eae34829131eaf1f151cb3dcaa5
ttppoo121440 <ttppoo121440@gmail.com>
2025-05-31 21:58:07 +1000

    feat: 新增 getRoomPlanDetailById 查詢房型計畫詳情功能

--- src/features/roomPlan/roomPlan.repo.ts
+++ src/features/roomPlan/roomPlan.repo.ts
@@ -1,6 +1,8 @@
 import { and, eq, SQL, sql } from 'drizzle-orm';
 
 import { db } from '@/config/database';
+import { hotel_rooms } from '@/database/schemas/hotel_rooms.schema';
+import { hotels } from '@/database/schemas/hotels.schema';
 import {
   DeleteRoomPlan,
   InsertRoomPlan,
@@ -9,8 +11,11 @@ import {
   SelectRoomPlanPrice,
   UpdateRoomPlan,
 } from '@/database/schemas/room_plans.schema';
+import { room_types } from '@/database/schemas/room_types.schema';
 import { BaseRepository } from '@/repositories/base-repository';
 import { PaginationType } from '@/types/pagination';
+
+import { getRoomPlanDetailByIdType } from './roomPlan.schema';
 
 function buildRoomPlanConditions(conditions: Partial<{ id: string; hotelId: string }>): SQL[] {
   const queryConditions: SQL[] = [];
@@ -86,5 +91,44 @@ export class RoomPlanRepo extends BaseRepository {
       .from(room_plans)
       .where(eq(room_plans.id, id));
     return result;
+  }
+  async getRoomPlanDetailById(roomPlanId: string): Promise<getRoomPlanDetailByIdType | null> {
+    const result = await db
+      .select({
+        hotel_id: room_plans.hotel_id,
+        hotel_name: hotels.name,
+        hotel_region: hotels.region,
+        hotel_address: hotels.address,
+        hotel_phone: hotels.phone,
+        hotel_facilities: hotels.hotel_facilities,
+        hotel_policies: hotels.hotel_policies,
+        hotel_cover_image: hotels.image_url,
+        transportation: hotels.transportation,
+        latitude: hotels.latitude,
+        longitude: hotels.longitude,
+
+        room_plan_id: room_plans.id,
+        subscription_price: room_plans.subscription_price,
+        price: room_plans.price,
+        start_time: room_plans.start_date,
+        end_time: room_plans.end_date,
+        plan_images: room_plans.images,
+
+        hotel_room_id: hotel_rooms.id,
+        hotel_room_description: hotel_rooms.description,
+        hotel_room_images: hotel_rooms.images,
+        base_price: hotel_rooms.basePrice,
+
+        room_type_id: room_types.id,
+        room_type_name: room_types.name,
+        room_type_description: room_types.description,
+        room_services: room_types.room_service,
+      })
+      .from(room_plans)
+      .innerJoin(hotels, eq(room_plans.hotel_id, hotels.id))
+      .innerJoin(hotel_rooms, eq(room_plans.hotel_id, hotel_rooms.hotel_id))
+      .innerJoin(room_types, eq(hotel_rooms.room_type_id, room_types.id))
+      .where(and(eq(room_plans.id, roomPlanId), eq(room_plans.is_active, true)));
+    return result[0] ?? null;
   }
 }
