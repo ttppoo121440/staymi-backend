2723f1c776a3a6d688d6336e271504bfa2e40ace
ttppoo121440 <ttppoo121440@gmail.com>
2025-05-31 21:55:37 +1000

    feat: 新增查詢計畫伴手禮的 controller 方法

---
+++ src/features/orderRoomProductItem/orderRoomProductItem.controller.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+
+import { HttpStatus } from '@/types/http-status.enum';
+import { appError } from '@/utils/appError';
+import { successResponse } from '@/utils/appResponse';
+
+import { OrderRoomProductItemRepo } from '../orderRoomProductItem/orderRoomProductItem.repo';
+
+import { productPlansSchema } from './orderRoomProductItem.schema';
+
+export class OrderRoomProductItemController {
+  constructor(private orderRoomProductItemRepo = new OrderRoomProductItemRepo()) {}
+  getByHotelProduct = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const hotelId = req.params.id;
+    if (!hotelId) {
+      return next(appError('缺少必要的參數: id', HttpStatus.BAD_REQUEST));
+    }
+    const result = await this.orderRoomProductItemRepo.getByHotelProduct(hotelId);
+    if (result.length === 0) {
+      return next(appError('沒有找到相關的計畫伴手禮', HttpStatus.NOT_FOUND));
+    }
+    const productPlansFilter = result
+      .filter((item) => item.id && item.product_id)
+      .map((item) => productPlansSchema.parse(item));
+    res.status(HttpStatus.OK).json(successResponse({ productPlans: productPlansFilter }, '取得計畫伴手禮清單成功'));
+  });
+  getByHotelProductItem = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const hotelId = req.params.id;
+    const productId = req.params.productId;
+
+    if (!hotelId || !productId) {
+      return next(appError('缺少必要的參數: hotelId 或 productId', HttpStatus.BAD_REQUEST));
+    }
+    const result = await this.orderRoomProductItemRepo.getByHotelProductItem(hotelId, productId);
+    if (!result) {
+      return next(appError('沒有找到相關的計畫伴手禮', HttpStatus.NOT_FOUND));
+    }
+    const productPlansFilter = productPlansSchema.parse(result);
+    res.status(HttpStatus.OK).json(successResponse({ productPlans: productPlansFilter }, '取得計畫伴手禮成功'));
+  });
+}
