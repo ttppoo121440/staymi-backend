bf80ef39b08d36afa02cdc4f78bf2d395bde4aa6
ttppoo121440 <ttppoo121440@gmail.com>
2025-05-31 21:51:39 +1000

    feat: hotels schema transportation、hotel_policies 欄位型別改為 text

--- src/database/schemas/hotels.schema.ts
+++ src/database/schemas/hotels.schema.ts
@@ -1,4 +1,4 @@
-import { boolean, decimal, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { boolean, decimal, pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 
 import { brand } from './brand.schema';
 
@@ -11,11 +11,12 @@ export const hotels = pgTable('hotels', {
   name: varchar('name', { length: 50 }).notNull().unique(),
   address: varchar('address', { length: 100 }).notNull(),
   phone: varchar('phone', { length: 20 }).notNull(),
-  transportation: varchar('transportation', { length: 255 }).notNull(),
-  hotel_policies: varchar('hotel_policies', { length: 255 }).notNull(),
+  transportation: text().notNull(),
+  hotel_policies: text().notNull(),
   latitude: decimal('latitude', { precision: 10, scale: 6 }).notNull(), // 精確到小數點後 6 位
   longitude: decimal('longitude', { precision: 10, scale: 6 }).notNull(), // 精確到小數點後 6 位
   hotel_facilities: varchar('hotel_facilities', { length: 50 }).array().notNull(),
+  image_url: varchar('image_url', { length: 255 }).notNull(),
   is_active: boolean('is_active').notNull().default(true),
   created_at: timestamp('created_at').defaultNow(),
   updated_at: timestamp('updated_at').defaultNow(),
